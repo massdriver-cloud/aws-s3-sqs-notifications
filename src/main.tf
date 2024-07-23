0fe77e6d0cb079e8812a8e46902d9f8214f29801
Cory O'Daniel <cory@coryodaniel.com>
2024-07-23 10:47:31 -0700

    bind policy

--- src/main.tf
+++ src/main.tf
@@ -2,6 +2,11 @@ resource "aws_sqs_queue" "main" {
   name = var.md_metadata.name_prefix
 }
 
+resource "aws_sqs_queue_policy" "main" {
+  queue_url = aws_sqs_queue.main.id
+  policy    = data.aws_iam_policy_document.main.json
+}
+
 resource "aws_s3_bucket_notification" "main" {
   bucket = element(
     reverse(
